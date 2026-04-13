9c09c42de205d395d5e25fbbe08a2c1eaa401983
evecus <jamie55@163.com>
2026-04-13 17:03:29 +0800

    Update Button.tsx

--- src/screens/Home/Views/Setting/components/Button.tsx
+++ src/screens/Home/Views/Setting/components/Button.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react'
 
 import TVButton from '@/components/common/TVButton'
+import { setFocusZone } from '@/screens/Home/TV/index'
 import Text from '@/components/common/Text'
 import { useTheme } from '@/store/theme/hook'
 import { createStyle } from '@/utils/tools'
@@ -21,6 +22,7 @@ export default memo(({ disabled, onPress, children, hasTVPreferredFocus, style }
     <TVButton
       style={[{ ...styles.button, backgroundColor: theme['c-button-background'] }, style]}
       onPress={onPress}
+      onFocus={() => setFocusZone('content')}
       disabled={disabled}
       hasTVPreferredFocus={hasTVPreferredFocus}
       borderRadius={4}
